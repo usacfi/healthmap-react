0cc2423b7a1fe9f5ff63e475fa2a767be4b663b2
Nelo Aguila <raphaelnelo.aguila@gmail.com>
2025-07-13 19:56:15 +0800

    fixed indentation

--- src/component/healthMap.tsx
+++ src/component/healthMap.tsx
@@ -29,16 +29,16 @@ const healthFacilities = Object.values(healthFacilitiesType[0]).flat()
 
 export default function HealthMap() {
 	// Initializing the map and the current location of the user
-    const [currentLocation, setCurrentLocation] = useState<google.maps.LatLngLiteral | google.maps.LatLng>({ lat: 11.0050, lng: 122.5373 });
-    const [mapCenter, setMapCenter] = useState<google.maps.LatLngLiteral>();
-    const [loadingState, setLoadingState] = useState(true);
-	const [userLocation, setUserLocation] = useState<google.maps.LatLngLiteral | google.maps.LatLng>({ lat: 11.0050, lng: 122.5373 })
+	const [currentLocation, setCurrentLocation] = useState<google.maps.LatLngLiteral | google.maps.LatLng>({ lat: 11.0050, lng: 122.5373 });
+	const [mapCenter, setMapCenter] = useState<google.maps.LatLngLiteral>();
+	const [loadingState, setLoadingState] = useState(true);
+	const [userLocation, setUserLocation] = useState<google.maps.LatLngLiteral | google.maps.LatLng>({ lat: 11.0050, lng: 122.5373 });
 
 	// The radius (circle) for proximity buffer, increase or decrease to liking
 	var radiusNum = 2000;	
 	const [radius, setRadius] = useState(radiusNum);
-	var radiusKM = radius/1000 // convert to KM
-	const [markers, setMarkers] = useState<{ name: string; healthResourceType: string; address: string; latitude: number; longitude: number}[]>([])
+	var radiusKM = radius/1000; // convert to KM
+	const [markers, setMarkers] = useState<{ name: string; healthResourceType: string; address: string; latitude: number; longitude: number}[]>([]);
 
 	// this the zoom of the map. If the map did not load it will automatically zoom out to show the whole region
 	const zoomLoad = (currentLocation: google.maps.LatLng | google.maps.LatLngLiteral) => currentLocation.lat === 11.0050 && currentLocation.lng === 122.5373 ? 9 : 14.5;
@@ -46,52 +46,55 @@ export default function HealthMap() {
 	// conditionals in changing the color relative to the radius
 	const reachTextChange = (radius: number) => 
 		radius <= radiusNum ? 'Walkable' : 
-		radius > radiusNum && radius <= 10000 ? 'Commute' : 
-		'Too Far';
+			radius > radiusNum && radius <= 10000 ? 'Commute' : 
+				'Too Far';
 	  
-	  const getFillColor = (radius: number) => 
+	const getFillColor = (radius: number) => 
 		radius <= radiusNum ? '#3b82f6' : 
-		radius > radiusNum && radius <= 10000 ? '#ffd55c' : 
-		'#f44336';
-	  
-	  const getStrokeColor = (radius: number) => 
+			radius > radiusNum && radius <= 10000 ? '#ffd55c' : 
+				'#f44336';
+	
+	const getStrokeColor = (radius: number) => 
 		radius <= radiusNum ? '#0c4cb3' : 
-		radius > radiusNum && radius <= 10000 ? '#4c3f1b' : 
-		'#300d0a';
+			radius > radiusNum && radius <= 10000 ? '#4c3f1b' : 
+				'#300d0a';
 	  
-
-	const map = useMap()
+	const map = useMap();
 
 	// Initializing the where the Map would zoom and the coordiantes of the current location
 	const changeCenter = (newCenter: google.maps.LatLng | null) => {
 		if (!newCenter) return;
+
 		setMapCenter({ lat: newCenter.lat(), lng: newCenter.lng() });
 		setCurrentLocation({lng: newCenter.lng(), lat: newCenter.lat()});
 	};
 
 	
-    const getCurrentLocation = () => {
-        navigator.geolocation.getCurrentPosition(
-        (position) => {
-            setMapCenter({ lat: position.coords.latitude, lng: position.coords.longitude});
-            setCurrentLocation({ lat: position.coords.latitude, lng: position.coords.longitude})
-			setUserLocation ({ lat: position.coords.latitude, lng: position.coords.longitude})
-            setLoadingState(false);
-        },
-        () => {
-		<Alert variant={'warning'} dismissible={true}>
-			Geolocation cannot detect location... Setting your current location to Western Visayas
-        </Alert>
-		setMapCenter({ lat: 11.0050, lng: 122.5373 });
-		setLoadingState(false);
-        });
-      };
+	const getCurrentLocation = () => {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+					setMapCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+					setCurrentLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
+					setUserLocation ({ lat: position.coords.latitude, lng: position.coords.longitude });
+					setLoadingState(false);
+			},
+			() => {
+				<Alert variant={'warning'} dismissible={true}>
+					Geolocation cannot detect location... Setting your current location to Western Visayas
+				</Alert>
+
+				setMapCenter({ lat: 11.0050, lng: 122.5373 });
+				setLoadingState(false);
+			}
+		);
+	};
 
 	const geometryLibrary = useMapsLibrary("geometry");
+
 	const [geometryLib] = useState<google.maps.GeometryLibrary['spherical'] | null>(null);
 
 	const [facilityCount, setFacilityCount] = useState(0); // Store the facility count
-	
+
 	const [selectedHealthResourceType, setSelectedHealthResourceType] = useState('All');
 
 	const healthResourceTypes = healthFacilitiesType.length > 0 ? Object.keys(healthFacilitiesType[0]) : [];
@@ -101,31 +104,42 @@ export default function HealthMap() {
 	};
 
 	// Markers within the access radius
-	useEffect(() => {
-		if (!geometryLibrary) return;
-		const markersWithinRadius = healthFacilities.filter((resource) => {
-			const distance = geometryLibrary?.spherical.computeDistanceBetween(
-				new google.maps.LatLng(currentLocation),
-				new google.maps.LatLng(resource.latitude, resource.longitude)
-			)
-			;
-		return distance <= radius;
-		});
-		// Filter based on selected facility type
-		const filteredFacilities = selectedHealthResourceType === 'All' 
-        ? markersWithinRadius 
-        : markersWithinRadius.filter((facility) => facility.healthResourceType === selectedHealthResourceType);
+	useEffect(
+		() => {
+			if (!geometryLibrary) return;
 
-		setMarkers(filteredFacilities);
-		setFacilityCount(filteredFacilities.length); // Update the count
-	}, [geometryLibrary, currentLocation, radius, selectedHealthResourceType, healthFacilities]);
+			const markersWithinRadius = healthFacilities.filter((resource) => {
+				const distance = geometryLibrary?.spherical.computeDistanceBetween(
+					new google.maps.LatLng(currentLocation),
+					new google.maps.LatLng(resource.latitude, resource.longitude)
+				);
+			
+				return distance <= radius;
+			});
+			
+			// Filter based on selected facility type
+			const filteredFacilities = selectedHealthResourceType === 'All' 
+				? markersWithinRadius 
+				: markersWithinRadius.filter((facility) => facility.healthResourceType === selectedHealthResourceType);
+
+			console.log(filteredFacilities);
+			filteredFacilities.forEach((k) => { console.log(k); console.log(getMarkerColor(k.healthResourceType)); });
+
+			setMarkers(filteredFacilities);
+			setFacilityCount(filteredFacilities.length); // Update the count
+		}, 
+		[geometryLibrary, currentLocation, radius, selectedHealthResourceType, healthFacilities]
+	);
 	
-	useEffect(() => {
-		getCurrentLocation()
-	}, []);
+	useEffect(
+		() => {
+			getCurrentLocation();
+		}, []
+	);
 
 	// info window
 	const [infowindowOpen, setInfowindowOpen] = useState(false);
+
 	const [selectedMarker, setSelectedMarker] = useState<{
 		name: string;
 		healthResourceType: string;
@@ -133,7 +147,7 @@ export default function HealthMap() {
 		latitude: number;
 		longitude: number;
 	  } | null>(null);
-  	const [markerRef, markerAnchor] = useAdvancedMarkerRef();
+	const [markerRef, markerAnchor] = useAdvancedMarkerRef();
 
 	const handleMarkerClick = (marker: {name: string, healthResourceType: string, address: string, latitude: number, longitude: number}) => {
 		setInfowindowOpen(true);
@@ -146,6 +160,7 @@ export default function HealthMap() {
 
 	// autocomplete
 	const [selectedPlace, setSelectedPlace] = useState<google.maps.places.PlaceResult | null>(null);
+
 	const destinationLocation = selectedPlace?.geometry?.location;
 
 	const [selectedCommunity, setSelectedCommunity] = useState<Community | null>(null);
@@ -156,20 +171,20 @@ export default function HealthMap() {
 			map.panTo({ lat: community.latitude, lng: community.longitude });
 			map.setZoom(14.5)
 		}
-	  };
+	};
 
 	useEffect(() => {
 		if (selectedCommunity) {
-		  	setCurrentLocation({ lat: selectedCommunity.latitude, lng: selectedCommunity.longitude });
-		  	setMapCenter({ lat: selectedCommunity.latitude, lng: selectedCommunity.longitude });
+			setCurrentLocation({ lat: selectedCommunity.latitude, lng: selectedCommunity.longitude });
+			setMapCenter({ lat: selectedCommunity.latitude, lng: selectedCommunity.longitude });
 		}
 	}, [selectedCommunity]);
 
 	const filteredMarkers = useMemo(() => {
 		if (selectedHealthResourceType === 'All') {
-		return markers;
+			return markers;
 		} else {
-		return markers.filter((marker) => marker.healthResourceType === selectedHealthResourceType);
+			return markers.filter((marker) => marker.healthResourceType === selectedHealthResourceType);
 		}
 	}, [markers, selectedHealthResourceType]);
 
@@ -210,179 +225,195 @@ export default function HealthMap() {
 		  default:
 			return '#3b82f6';
 		}
-	  };
+	};
 
 	if (loadingState === true) {
 		return (
 			<div className="center">
-					<Spinner animation="border" role="status"> </Spinner>
+				<Spinner animation="border" role="status"> </Spinner>
 			</div>
 		);
 	};
 
-    return (
-	<div className="container">
-		<div className='controls'>
-			<div className="logo-title">
-				<img className='logo' src={Logo} alt='Logo of Health Map'/>
-				<h1 className="title">Health Map</h1>
-			</div>
+	return (
+		<div className="container">
+			<div className='controls'>
+				<div className="logo-title">
+					<img className='logo' src={Logo} alt='Logo of Health Map'/>
+					<h1 className="title">Health Map</h1>
+				</div>
 
-			<h3>Search Facilities</h3>			
-			{map && (
-				<AutocompleteSearch onPlaceSelect={setSelectedPlace} selectedPlace={destinationLocation} currentLocation={currentLocation}/>)}
-	
-			{map && selectedPlace && (
-				<Directions currentLocation={currentLocation} selectedPlace={destinationLocation} map={map} />)}
+				<h3>Search Facilities</h3>
 
-			{/* <h2>Access Range</h2> */}
-			<div className="dist-time" style={{marginTop: 30}}>
-				<div className="label">Access Range</div>
-				<div className="value">{radiusKM.toFixed(2)} km</div>
-			</div>
+				{
+					map 
+					&& (<AutocompleteSearch onPlaceSelect={setSelectedPlace} selectedPlace={destinationLocation} currentLocation={currentLocation}/>)
+				}
+		
+				{
+					map 
+					&& selectedPlace 
+					&& (<Directions currentLocation={currentLocation} selectedPlace={destinationLocation} map={map} />)
+				}
 
-			<div className="dist-time" style={{marginBottom: 30}}>
-				<div className="label">Reachability</div>
-				<div style={{ display: 'flex', alignItems: 'center' }}>
-					<span>{reachTextChange(radius)}</span>
-					<div style={{ backgroundColor: getFillColor(radius), width: 20, height: 20, borderRadius: 10, marginLeft: 10 }} />
+				{/* <h2>Access Range</h2> */}
+				<div className="dist-time" style={{marginTop: 30}}>
+					<div className="label">Access Range</div>
+					<div className="value">{radiusKM.toFixed(2)} km</div>
+				</div>
+
+				<div className="dist-time" style={{marginBottom: 30}}>
+					<div className="label">Reachability</div>
+					<div style={{ display: 'flex', alignItems: 'center' }}>
+						<span>{reachTextChange(radius)}</span>
+						<div style={{ backgroundColor: getFillColor(radius), width: 20, height: 20, borderRadius: 10, marginLeft: 10 }} />
+					</div>
+				</div>
+
+				<h3>Health Facilities Type</h3>
+				<div className="dist-time">
+					<div className="label">Facilities in Range</div>
+					<div className="value">{facilityCount}</div>
+				</div>
+
+				<select className='dropdown-facilities' value={selectedHealthResourceType} onChange={handleHealthResourceTypeChange}>
+					<option value="All">All Health Facilities</option>
+					{
+						healthResourceTypes.map((type) => (
+							<option key={type} value={type}>
+								{type}
+							</option>
+						))
+					}
+				</select>
+				
+				<div>
+					<h3 className='inline-element'>Communities</h3>
+					<button className='button-location' onClick={() => {
+						if (map) {
+							setCurrentLocation(userLocation)
+							map.panTo(currentLocation);
+							map.setZoom(16);
+						}
+					}}>					
+						📍
+					</button> 
+				</div>
+				<div className='community-panel'>
+					<DropdownList 
+						data={communityData} 
+						dataKey='name' 
+						textField='name' 
+						placeholder='Select Community'
+						filter={false}
+						onSelect={(community) => handleCommunitySelect(community)}
+					/>
+					{/* <div  className='button-stack'>
+						{communityData.map((community, index) => (
+							<button className='button-communities' key={index} onClick={() => handleCommunitySelect(community)}>
+							{community.name}
+							</button>
+						))}
+					</div> */}
 				</div>
 			</div>
-
-			<h3>Health Facilities Type</h3>
-			<div className="dist-time">
-				<div className="label">Facilities in Range</div>
-				<div className="value">{facilityCount}</div>
-			</div>
-
-			<select className='dropdown-facilities' value={selectedHealthResourceType} onChange={handleHealthResourceTypeChange}>
-				<option value="All">All Health Facilities</option>
-				{healthResourceTypes.map((type) => (
-					<option key={type} value={type}>
-					{type}
-					</option>
-				))}
-			</select>
-			
-			<div>
-				<h3 className='inline-element'>Communities</h3>
-				<button className='button-location' onClick={() => {
-					if (map) {
-					setCurrentLocation(userLocation)
-					map.panTo(currentLocation);
-					map.setZoom(16);
-					}
-				}}>					
-					📍
-				</button> 
-			</div>
-			<div className='community-panel'>
-				<DropdownList 
-					data={communityData} 
-					dataKey='name' 
-					textField='name' 
-					placeholder='Select Community'
-					filter={false}
-					onSelect={(community) => handleCommunitySelect(community)}
-				/>
-				{/* <div  className='button-stack'>
-					{communityData.map((community, index) => (
-						<button className='button-communities' key={index} onClick={() => handleCommunitySelect(community)}>
-						{community.name}
-						</button>
-					))}
-				</div> */}
-			</div>
-		</div>
-			<div className='map-field'>
-			<Map
-				mapId={process.env.REACT_APP_GOOGLE_MAPS_ID}
-				defaultCenter={mapCenter}
-				defaultZoom={zoomLoad(currentLocation)}
-				disableDefaultUI={false}
-				streetViewControl={true}
-				mapTypeControl={false}
-			>
-				<AdvancedMarker
-				position={currentLocation}
-				draggable={true}
-				onDrag={e =>
-					setCurrentLocation({lat: e.latLng?.lat() ?? 0, lng: e.latLng?.lng() ?? 0})
-				}
+				<div className='map-field'>
+				<Map
+					mapId={process.env.REACT_APP_GOOGLE_MAPS_ID}
+					defaultCenter={mapCenter}
+					defaultZoom={zoomLoad(currentLocation)}
+					disableDefaultUI={false}
+					streetViewControl={true}
+					mapTypeControl={false}
 				>
-					<div style={{
-						width: 16,
-						height: 16,
-						position: 'absolute',
-						top: 0,
-						left: 0,
-						background: '#3b82f6',
-						border: '2px solid #0c4cb3',
-						borderRadius: '50%',
-						transform: 'translate(-50%, -50%)'
-						}}>	
-					</div>
-				</AdvancedMarker>
+					<AdvancedMarker
+						position={currentLocation}
+						draggable={true}
+						onDrag={e =>
+							setCurrentLocation({lat: e.latLng?.lat() ?? 0, lng: e.latLng?.lng() ?? 0})
+						}
+					>
+						<div style={{
+							width: 16,
+							height: 16,
+							position: 'absolute',
+							top: 0,
+							left: 0,
+							background: '#3b82f6',
+							border: '2px solid #0c4cb3',
+							borderRadius: '50%',
+							transform: 'translate(-50%, -50%)'
+							}}>	
+						</div>
+					</AdvancedMarker>
 
-				<Circle
-				radius={radiusNum}
-				center={currentLocation}
-				onRadiusChanged={setRadius}
-				onCenterChanged={changeCenter}
-				strokeColor={getStrokeColor(radius)}
-				strokeOpacity={1}
-				strokeWeight={2}
-				fillColor={getFillColor(radius)}
-				fillOpacity={0.3}
-				editable
-				draggable
-				/>
-				{filteredMarkers.map((marker, index) => (
-					<>
-						<AdvancedMarker
-						key={index}
-						ref={markerRef}
-						position={{
-							lat: marker.latitude,
-							lng: marker.longitude,
-						}}
-						collisionBehavior={CollisionBehavior.REQUIRED_AND_HIDES_OPTIONAL}
-						onClick={() => handleMarkerClick(marker)}
-						title={marker.name}
-						>
-							<div
-								style={{
-								width: 12,
-								height: 12,
-								position: 'absolute',
-								top: 0,
-								left: 0,
-								background: getMarkerColor(marker.healthResourceType),
-								border: '1px solid #c3c3c3',
-								borderRadius: '50%',
-								transform: 'translate(-50%, -50%)',
-								}}
-							/>
-						</AdvancedMarker>
+					<Circle
+						radius={radiusNum}
+						center={currentLocation}
+						onRadiusChanged={setRadius}
+						onCenterChanged={changeCenter}
+						strokeColor={getStrokeColor(radius)}
+						strokeOpacity={1}
+						strokeWeight={2}
+						fillColor={getFillColor(radius)}
+						fillOpacity={0.3}
+						editable
+						draggable
+					/>
 
-						{infowindowOpen && selectedMarker === marker && (
-							<InfoWindow
-								position={{lat: marker.latitude, lng: marker.longitude}}
-								maxWidth={500}
-								onCloseClick={handleClose}>
-								<h3>{marker.name}</h3>
-								<i>{marker.address}</i>
-								<p>{marker.healthResourceType}</p>
-								<p>Operation Hours: </p>
-								<p>Contact Details: </p>
-								<p>Services Offered: </p>
-							</InfoWindow>
-						)} 
-					</>
-				))}
-			</Map>
-			<MapHandler place={selectedPlace} />
+					{
+						filteredMarkers.map((marker, index) => (
+							<>
+								<AdvancedMarker
+									key={index}
+									ref={markerRef}
+									position={{
+										lat: marker.latitude,
+										lng: marker.longitude,
+									}}
+									collisionBehavior={CollisionBehavior.REQUIRED_AND_HIDES_OPTIONAL}
+									onClick={() => handleMarkerClick(marker)}
+									title={marker.name}
+								>
+									<div
+										style={{
+										width: 12,
+										height: 12,
+										position: 'absolute',
+										top: 0,
+										left: 0,
+										background: getMarkerColor(marker.healthResourceType),
+										border: '1px solid #c3c3c3',
+										borderRadius: '50%',
+										transform: 'translate(-50%, -50%)',
+										}}
+									/>
+								</AdvancedMarker>
+
+								{
+									infowindowOpen 
+									&& selectedMarker === marker 
+									&& (
+										<InfoWindow
+											position={{lat: marker.latitude, lng: marker.longitude}}
+											maxWidth={500}
+											onCloseClick={handleClose}
+										>
+											<h3>{marker.name}</h3>
+											<i>{marker.address}</i>
+											<p>{marker.healthResourceType}</p>
+											<p>Operation Hours: </p>
+											<p>Contact Details: </p>
+											<p>Services Offered: </p>
+										</InfoWindow>
+									)
+								} 
+							</>
+						))
+					}
+				</Map>
+				<MapHandler place={selectedPlace} />
+			</div>
 		</div>
-	</div>
-    );
-  }
+	);
+}
